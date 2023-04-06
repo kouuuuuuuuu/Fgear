bfe6782aca6491a2cd5789a13e06e845adba1e9f
NguyenThinh <koulse.kudaza@gmail.com>
2023-04-06 16:15:08 +0700

    First step

---
+++ src/main/java/com/fgear/webapp/FgearApplication.java
@@ -0,0 +1,13 @@
+package com.fgear.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FgearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FgearApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fgear/webapp/FgearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fgear.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FgearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
